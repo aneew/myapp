d43d6068134864aabbbc26a114dac9992c30bc63
Aneew <d.gagyor@seznam.cz>
2024-01-23 12:55:10 +0100

    navigate main button working

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+
+  constructor(private router: Router) {}
+
+  goToExplorePage() {
+    this.router.navigate(['/tabs/explore']);
+  }
+
+  goToAboutPage() {
+    this.router.navigate(['/tabs/about']);
+  }
+
+  goToMyBookPage() {
+    this.router.navigate(['/tabs/mybook']);
+  }
 
   ngOnInit() {
   }
